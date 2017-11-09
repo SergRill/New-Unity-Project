5348aa075b2e5e3ca0e7af583d01ec0980ebf8fd
SergRill <32397297+SergRill@users.noreply.github.com>
2017-11-09 13:48:07 +0300

    dsggrg
    sdfdsfsdfsed

--- Assets/Resourses/Scripts/Inventory/InventoryButton.cs
+++ Assets/Resourses/Scripts/Inventory/InventoryButton.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class InventoryButton : MonoBehaviour {
 
-    public SpriteRenderer background;
-    public SpriteRenderer objectSprite;
+   // public SpriteRenderer background;
+   // public SpriteRenderer objectSprite;
 
     public Inventory inventory;
     public int id;
@@ -29,11 +29,11 @@ public class InventoryButton : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        int x = ((Screen.width - inventory.indent*2 ) / cells) * (id + 1) ;
-        int d = ((Screen.width - inventory.indent * 2) / cells)/2;
+       // int x = ((Screen.width - inventory.indent*2 ) / cells) * (id + 1) ;
+       // int d = ((Screen.width - inventory.indent * 2) / cells)/2;
 
+     
 
-
-        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x - d + inventory.indent, 50 , 100));
+       // transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x - d + inventory.indent, 50 , 100));
 	}
 }
