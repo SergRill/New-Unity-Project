ec6d70dd15935c2ee207b4d42f1781bcb5225507
MoeImya <32247140+MoeImya@users.noreply.github.com>
2017-11-17 00:08:23 +0400

    First "Ideas Interface" update
    start develop interfave of main character "ideas". First Update

---
+++ Assets/Resourses/Scripts/Interface/QuestInterface/IdeaScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdeaScript : MonoBehaviour {
+
+   // public Color color;
+    public string text;
+    public bool isImportant = false;
+    public double minAlphaValue = 0.5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+}
