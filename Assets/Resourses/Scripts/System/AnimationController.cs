fe324198a52fd2ed47a3e5a3f3c5c7445c252553
MoeImya <32247140+MoeImya@users.noreply.github.com>
2017-12-10 22:19:50 +0400

    Up

--- Assets/Resourses/Scripts/System/AnimationController.cs
+++ Assets/Resourses/Scripts/System/AnimationController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour {
 
     bool isLoop = false;
-    bool isPlaying = false;
-
     Animator animator;
+
+    public AnimationBase animationBase;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
 	}
+
+    public void startAnimation(int id, int delay, bool isLoop)
+    {
+        if(animator != null)
+        {
+            animator.CrossFade(animationBase.animations[id], delay);
+            this.isLoop = isLoop;
+        }
+    }
 
     public void startAnimation(string name, int delay, bool isLoop)
     {
